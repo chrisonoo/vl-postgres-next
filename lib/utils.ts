7e455bdec885435fa4aa12407c16bc78b8598e40
chrisonoo <chris@onoo.no>
2024-10-07 08:00:49 +0200

    Refactor utils.ts to improve code readability and database handling

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,20 @@
-import ms from 'ms'
+import ms from "ms";
+import { Pool } from "pg";
+import { sql } from "@vercel/postgres";
 
 export const timeAgo = (timestamp: Date, timeOnly?: boolean): string => {
-  if (!timestamp) return 'never'
+  if (!timestamp) return "never";
   return `${ms(Date.now() - new Date(timestamp).getTime())}${
-    timeOnly ? '' : ' ago'
-  }`
+    timeOnly ? "" : " ago"
+  }`;
+};
+
+export function getDatabase() {
+  if (process.env.VERCEL) {
+    return sql;
+  } else {
+    return new Pool({
+      connectionString: process.env.DATABASE_URL,
+    });
+  }
 }
