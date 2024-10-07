8bb19b2b0a461e127552fbcb61624e3b871315cb
chrisonoo <chris@onoo.no>
2024-10-07 10:38:26 +0200

    Refactor next.config.js to remove experimental server components and configure image domain whitelist

--- next.config.js
+++ next.config.js
@@ -1,17 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["@vercel/postgres"],
-  },
-  webpack: (config, { isServer, dev }) => {
-    if (!isServer && !dev) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        pg: false,
-      };
-    }
-    return config;
-  },
   images: {
     domains: ["images.ctfassets.net"],
   },
